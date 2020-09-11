61614eb68693997dd6a109fee0b8e6c7b98978a7
Anurag <anuragagrawal4998@gmail.com>
2020-09-12 00:58:04 +0530

    footer

--- src/App.js
+++ src/App.js
@@ -1,52 +1,52 @@
-import React, { useState, useEffect } from 'react';
-import './styles/styles.scss';
-import Header from './components/header';
-import Togglebutton from "./components/button"
-import Main from './components/Mainstat';
-import Table from './components/table';
-import Chart from './components/charts'
-import RecoveredChart from './components/reccharts'
-import DeceaseddChart from './components/deccharts'
+import React, { useState, useEffect } from "react";
+import "./styles/styles.scss";
+import Header from "./components/header";
+import Togglebutton from "./components/button";
+import Main from "./components/Mainstat";
+import Table from "./components/table";
+import Chart from "./components/charts";
+import RecoveredChart from "./components/reccharts";
+import DeceaseddChart from "./components/deccharts";
+import Footer from "./components/footer";
 
-import Jsoncontext from './context/context';
+import Jsoncontext from "./context/context";
 
-const url = "https://api.covid19india.org/data.json"
+const url = "https://api.covid19india.org/data.json";
 const requestOptions = {
-  method: 'GET',
-  redirect: 'follow'
+  method: "GET",
+  redirect: "follow",
 };
 
-
-
-
 function App() {
-
-  const [stateWise, setData] = useState()
-  const [total, setTotal] = useState()
-  const [flag, setToggle] = useState(0)
+  const [stateWise, setData] = useState();
+  const [total, setTotal] = useState();
+  const [flag, setToggle] = useState(0);
 
   useEffect(() => {
-    fetch(url, requestOptions).then(response => response.json())
-      .then(resp => {
+    fetch(url, requestOptions)
+      .then((response) => response.json())
+      .then((resp) => {
         for (let i = 1; i < 38; i++) {
           for (let j = 1; j < 38 - i; j++) {
-            if (parseInt(resp.statewise[j].confirmed) < parseInt(resp.statewise[j + 1].confirmed)) {
+            if (
+              parseInt(resp.statewise[j].confirmed) <
+              parseInt(resp.statewise[j + 1].confirmed)
+            ) {
               var temp;
-              temp = resp.statewise[j + 1]
-              resp.statewise[j + 1] = resp.statewise[j]
-              resp.statewise[j] = temp
+              temp = resp.statewise[j + 1];
+              resp.statewise[j + 1] = resp.statewise[j];
+              resp.statewise[j] = temp;
             }
-
           }
         }
-        setData(resp.statewise)
-        setTotal(resp.cases_time_series)
-      })
-  }, [])
+        setData(resp.statewise);
+        setTotal(resp.cases_time_series);
+      });
+  }, []);
 
   const handletoggle = (flag) => {
-    setToggle(flag)
-  }
+    setToggle(flag);
+  };
 
   return (
     <Jsoncontext.Provider value={{ stateWise, total }}>
@@ -64,6 +64,7 @@ function App() {
         <Chart />
         <RecoveredChart />
         <DeceaseddChart />
+        <Footer />
       </div>
     </Jsoncontext.Provider>
   );
---
+++ src/components/footer.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { RiSurgicalMaskLine } from "react-icons/ri";
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <div className="row mb-3">
+        <div className="col-12 align-items-center">
+          <div className="footer-text">
+            <p>
+              Made with care by <span>Anurag</span>{" "}
+            </p>
+            <span className="mask-icon">
+              {" "}
+              <RiSurgicalMaskLine />
+            </span>
+          </div>
+          <div className="footer-icons">
+            <a href=" https://github.com/anurag4998">
+              <FaGithub className="git" />
+            </a>
+            <a href="https://www.linkedin.com/in/anurag-agrawal-674406164/">
+              <FaLinkedin className="linkedin" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
